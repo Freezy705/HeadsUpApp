ec9feee4b0fffb1ea05fe0d5a2f855ba82fd19f3
Lewis <t7058263@live.tees.ac.uk>
2019-03-06 21:03:47 +0000

    Added functionality to User, altered styling slightly.

--- app/src/main/java/com/example/lewis/headsup/data/User.java
+++ app/src/main/java/com/example/lewis/headsup/data/User.java
@@ -1,6 +1,16 @@
 package com.example.lewis.headsup.data;
 
+import android.app.Activity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,8 +19,60 @@ import java.util.List;
 
 public class User implements Serializable{
 
-    private int taskNo, tasksCompleted, tasksFinished, tasksInProgress;
+    private int taskNo = 0, tasksCompleted = 0,
+                tasksFinished = 0, tasksInProgress = 0;
     private List<Task> tasks;
 
+    private User instance;
 
+    public User(){
+        tasks = new ArrayList<Task>();
+    }
+
+    public User getUser(Activity context){
+
+        if(instance == null){
+            loadUser(context);
+            if(instance == null)
+            instance = createNewUser(context);
+        }
+        return instance;
+    }
+
+    private User loadUser(Activity context){
+            try {
+                FileInputStream fileIn = new FileInputStream(context.getDir("user_inf", Activity.MODE_PRIVATE));
+                ObjectInputStream obj = new ObjectInputStream(fileIn);
+                instance = (User) obj.readObject();
+                obj.close();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+    return instance;
+    }
+
+    private User createNewUser(Activity context){
+
+        instance = new User();
+        updateUser(context);
+        return instance;
+
+    }
+
+
+
+    public void updateUser(Activity context){
+        FileOutputStream fileout = null;
+        try {
+            fileout = new FileOutputStream(context.getDir("user_inf", Activity.MODE_PRIVATE));
+            ObjectOutputStream obj = new ObjectOutputStream(fileout);
+            obj.writeObject(instance);
+            obj.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
